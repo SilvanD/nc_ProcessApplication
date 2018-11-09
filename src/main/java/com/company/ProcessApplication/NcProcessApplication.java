e02807c8e46b94cf2a0b1947725e308ebc87af9c
SilvanD <mikdruzhinin@gmail.com>
2018-11-09 13:54:25 +0400

    Initial commit

---
+++ src/main/java/com/company/ProcessApplication/NcProcessApplication.java
@@ -0,0 +1,12 @@
+package com.company.ProcessApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcProcessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcProcessApplication.class, args);
+    }
+}
---
+++ src/test/java/com/company/ProcessApplication/NcProcessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.ProcessApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NcProcessApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
